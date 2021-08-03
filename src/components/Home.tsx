6ae23f74b09a5bb5fd7aba1cbe9ae207cf885a61
Hiago Coelho <b8coelho@gmail.com>
2021-08-02 22:49:11 -0300

    fix: fixed Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Home: React.FC = () => {
-  return <Text>Hoeeeeeeeeme</Text>;
+  return <Text>Home</Text>;
 };
 
 export default Home;
